cebd5804e396525bff3185c9b5e350ebcb4a5ef4
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-10-03 02:13:26 +0200

    Fix: Solve any CSS problem

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ import '../styles/Card.css';
 export default function Card({name, imageUrl, id,handleClick}) {
 
     return (
-        <div className="card" data-id={id} onClick={handleClick}>
+        <button className="card" data-id={id} onClick={handleClick}>
             <img src={imageUrl} alt={name} />
              <span>{name}</span>
-        </div>
+        </button>
     )
 }
